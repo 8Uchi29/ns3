d21617ba14c8e20ae9ddcdc68d2ea6efb531fb5a
Ryo Uchiyama <r-uchiyama@wh.jp.nec.com>
2015-01-13 10:31:47 +0900

    モジュールの追加

---
+++ tcp-uchi/helper/tcp-uchi-helper.cc
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "tcp-uchi-helper.h"
+
+namespace ns3 {
+
+/* ... */
+
+
+}
+
---
+++ tcp-uchi/model/tcp-uchi.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TCP_UCHI_H
+#define TCP_UCHI_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* TCP_UCHI_H */
+
